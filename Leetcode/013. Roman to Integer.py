edb8e937a34f385d9c35af0dcc2409c161bcfe98
dddpros <imber.lemon09@gmail.com>
2025-09-24 08:52:20 +0300

    update 1

--- Leetcode/013. Roman to Integer.py
+++ Leetcode/013. Roman to Integer.py
@@ -1,5 +1,31 @@
-s = "MCMXCIV"
 def roman_to_int(s):
+    s_new = []
+    sum = 0
+    for i in range(0, len(s)):
+        if s[i] == "M":
+            s_new.append(1000)
+        elif s[i] == "D":
+            s_new.append(500)
+        elif s[i] == "C":
+            s_new.append(100)
+        elif s[i] == "L":
+            s_new.append(50)
+        elif s[i] == "X":
+            s_new.append(10)
+        elif s[i] == "V":
+            s_new.append(5)
+        elif s[i] == "I":
+            s_new.append(1)
+    n = 0
+    for i in range(0, len(s_new) - 1):
+        if s_new[i + 1] > s_new[i]:
+            sum += s_new[i + 1] - s_new[i]
+            n = 1
+        else:
+            sum += s_new[i]
+    if n == 1:
+        return sum
+    else:
+        return sum + s_new[len(s_new) - 1]
 
-
-print(roman_to_int(s))
+print(roman_to_int("MCMXCIV"))
--- Leetcode/020. Valid Parentheses.py
+++ Leetcode/020. Valid Parentheses.py
@@ -1,9 +1,10 @@
-s = "[()]"
-lst = ["(", ")", "[", "]", "{", "}"]
-s_new = []
-for i in range(0, len(s)):
-    if s[i] in lst:
-        s_new.append(lst.index(s[i]))
-print(s_new)
-for i in range(1, len(s_new)):
-    if
+def is_valid(s):
+    lst = ["(", ")", "[", "]", "{", "}"]
+    s_new = []
+    for i in range(0, len(s)):
+        if s[i] in lst:
+            s_new.append(lst.index(s[i]))
+    for i in range(1, len(s_new)):
+        if s_new[i-1] > s_new[i]:
+    return True
+print(is_valid("[()]"))
