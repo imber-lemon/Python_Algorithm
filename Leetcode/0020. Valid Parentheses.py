0fc7f3ec4bd6540dd1538e8c4eaa004e6bc80ad1
dddpros <imber.lemon09@gmail.com>
2025-09-30 08:46:41 +0300

    update 1

--- Leetcode/0020. Valid Parentheses.py
+++ Leetcode/0020. Valid Parentheses.py
@@ -5,10 +5,16 @@ def is_valid(s):
         if s[i] in lst:
             s_new.append(lst.index(s[i]))
     print(s_new)
+    j = 0
+    for k in range(len(s_new) - 1, len(s_new)//2, - 1):
+        if s_new[j] != s_new[k] - 1:
+            return False
+        else:
+            j += 1
     for i in range(1, len(s_new), 2):
         if s_new[i-1] > s_new[i]:
             return False
         elif s_new[i-1] + 1 != s_new[i]:
             return False
     return True
-print(is_valid(""))
+print(is_valid("{[]}"))
