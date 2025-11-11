b2f2f532e8db41b3b9a19470a47819083afe1ecf
dddpros <imber.lemon09@gmail.com>
2025-11-11 08:50:09 +0300

    update

---
+++ Other/2025_11_11.py
@@ -0,0 +1,32 @@
+def F(n):
+    if n == 1:
+        return 2
+    elif n > 1 and F(n - 1) < 7555444:
+        return F(n - 1) + 6
+    else:
+        return F(n - 1) - 7555444
+
+
+print(F(7555451))
+
+
+# f = [0] * 7555451
+# for n in range(0, 7555451):
+#     if n == 1:
+#         f[n] = 2
+#     elif n > 1 and f[n - 1] < 7555444:
+#         f[n] = f[n - 1] + 6
+#     else:
+#         f[n] = f[n - 1] - 7555444
+#
+# print(f[7555450])
+
+# lst = [6, 3, 4, 5, 7]
+# num = 4
+# for i in range(0, lst.index(num) - 1):
+#     if lst[i] > lst[i+1]:
+#         lst[i], lst[i+1] = lst[i+1], lst[i]
+# for i in range(lst.index(num), len(lst) - 1):
+#     if lst[i] > lst[i+1]:
+#         lst[i], lst[i+1] = lst[i+1], lst[i]
+# print(lst)
