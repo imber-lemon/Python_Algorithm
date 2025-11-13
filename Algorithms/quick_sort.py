ab66f43926a3d4a2cd4178fce93affa3911c9ccf
dddpros <imber.lemon09@gmail.com>
2025-11-13 08:53:04 +0300

    update

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -1,12 +1,11 @@
-import random
-def quicksort(lst):
+def q_sort(lst):
     if len(lst) <= 1:
         return lst
     else:
-        q = random.choice(lst)
+        q = lst[len(lst)//2]
         lower = [elem for elem in lst if elem < q]
         pivot = [q] * lst.count(q)
         higher = [elem for elem in lst if elem > q]
-        return quicksort(lower) + pivot + quicksort(higher)
+        return q_sort(lower) + pivot + q_sort(higher)
 
-print(quicksort([5, 3, 7, 2, 4]))
+print(q_sort([5, 3, 7, 2, 4]))
