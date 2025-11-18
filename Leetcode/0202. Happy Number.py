61b666157e34897773baefbbff6f35d5bdb8b5ed
dddpros <imber.lemon09@gmail.com>
2025-11-18 08:49:47 +0300

    update

--- Leetcode/0202. Happy Number.py
+++ Leetcode/0202. Happy Number.py
@@ -1,17 +1,17 @@
 def happy_num(n):
-    lst = set()
+    sums = set()
     sum = 0
-    lst.add(n)
-    while sum not in lst:
+    sums.add(n)
+    while sum not in sums:
         n = str(n)
         for i in range(0, len(n)):
             sum += int(n[i]) ** 2
         if sum == 1:
             return True
-        elif sum in lst:
+        elif sum in sums:
             return False
         else:
-            lst.add(sum)
+            sums.add(sum)
             n = sum
             sum = 0
     return False
