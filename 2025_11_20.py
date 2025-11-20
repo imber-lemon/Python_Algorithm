cc1f6ea61dc3b7a15e66dee61ab24c0d006b220a
dddpros <imber.lemon09@gmail.com>
2025-11-20 14:44:14 +0300

    update

---
+++ 2025_11_20.py
@@ -0,0 +1,8 @@
+def f(n):
+    if n > 10000:
+        return 42
+    elif n <= 10000 and n % 2 == 0:
+        return 2 * n + f(n + 3) + f(n + 4) + f(n + 6)
+    else:
+        return -(n + f(n + 1) + f(n + 3))
+print(f(9996) - f(9994))
