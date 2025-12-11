e5f0bb6518ea2101fb3132bfdda307c3c3213467
dddpros <imber.lemon09@gmail.com>
2025-12-11 08:50:20 +0300

    update

--- Leetcode/0227. Basic Calculator II.py
+++ Leetcode/0227. Basic Calculator II.py
@@ -1,23 +1,35 @@
 eq = '3+5*10'
-s = []
 s_ind = []
+s = []
 n = []
 nums = '0123456789'
 for i in range(len(eq)):
     if eq[i] not in nums:
         s.append(eq[i])
         s_ind.append(i)
-    # else:
-    #     x = 0
-    #     while eq[x] in nums:
-    #         num += eq[x]
-    #         x += 1
-    #     n.append(num)
-    #     num = ''
 i_last = 0
 for i in s_ind:
     n.append(eq[i_last:i])
     i_last = i + 1
 n.append(eq[i_last:len(eq)])
-print(n)
-print(s)
+res = 0
+for i in range(0, len(s)):
+    if s[i] == '*':
+        n[i] = int(n[i]) * int(n[i+1])
+        n.pop(i+1)
+        s.pop(i)
+    elif s[i] == '/':
+        n[i] = int(n[i]) // int(n[i+1])
+        n.pop(i+1)
+        s.pop(i)
+    if '*' and '/' not in s:
+        print(n)
+        print(s)
+        for i in range(0, len(s)):
+            if s[i] == '+':
+                res += int(n[i]) + int(n[i+1])
+                print(int(n[i]), int(n[i+1]),int(n[i]) + int(n[i+1]))
+            elif s[i] == '-':
+                res += int(n[i]) - int(n[i+1])
+
+print(res)
