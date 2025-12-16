dd94951c140f4e60847e3ef418e632bf3431dd59
dddpros <imber.lemon09@gmail.com>
2025-12-16 08:49:45 +0300

    update

--- Leetcode/0227. Basic Calculator II.py
+++ Leetcode/0227. Basic Calculator II.py
@@ -1,4 +1,4 @@
-eq = '4*3+10'
+eq = '3+5*10/2'
 s_ind = []
 s = []
 n = []
@@ -13,31 +13,35 @@ for i in s_ind:
     i_last = i + 1
 n.append(eq[i_last:len(eq)])
 res = 0
-for i in range(0, len(s)):
+print(n)
+print(s)
+
+i = 0
+while '*' in s and '/' in s:
     if s[i] == '*':
         n[i] = int(n[i]) * int(n[i+1])
         n.pop(i + 1)
         s.pop(i)
+        print(n)
     elif s[i] == '/':
         n[i] = int(n[i]) // int(n[i+1])
         n.pop(i + 1)
         s.pop(i)
-if '*' and '/' not in s:
-    print(n)
-    print(s)
-    for i in range(0, len(s)):
-        if s[i] == '+' and len(n) > 2:
-            res += int(n[i]) + int(n[i+1])
-            n.pop(i)
-            n.pop(i + 1)
-            print(res)
-        elif s[i] == '-' and len(n) > 2:
-            res += int(n[i]) - int(n[i+1])
-            n.pop(i)
-            n.pop(i + 1)
-            print(res)
-        if s[i] == '+' and len(n) <= 2:
-            res += int(n[0]) + int(n[1])
-        elif s[i] == '-' and len(n) <= 2:
-            res += int(n[0]) - int(n[1])
+    i += 1
+print(n)
+print(s)
+for i in range(0, len(s)):
+    if s[i] == '+' and len(n) > 2:
+        n[i] = int(n[i]) + int(n[i+1])
+        n.pop(i + 1)
+        print(n, s)
+    elif s[i] == '-' and len(n) > 2:
+        n[i] += int(n[i]) - int(n[i+1])
+        n.pop(i + 1)
+        print(n, s)
+
+    if s[i] == '+' and len(n) <= 2:
+        res += int(n[0]) + int(n[1])
+    elif s[i] == '-' and len(n) <= 2:
+        res += int(n[0]) - int(n[1])
 print(res)
