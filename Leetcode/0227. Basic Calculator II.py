99126a9a635b7122437a0ecd3547abca7f14f8b0
dddpros <imber.lemon09@gmail.com>
2025-12-16 11:47:09 +0300

    update

--- Leetcode/0227. Basic Calculator II.py
+++ Leetcode/0227. Basic Calculator II.py
@@ -1,49 +1,61 @@
-eq = '3+5*10/2'
-s_ind = []
-s = []
-n = []
-nums = '0123456789'
-for i in range(len(eq)):
-    if eq[i] not in nums:
-        s.append(eq[i])
-        s_ind.append(i)
-i_last = 0
-for i in s_ind:
-    n.append(eq[i_last:i])
-    i_last = i + 1
-n.append(eq[i_last:len(eq)])
-res = 0
-print(n)
-print(s)
+from unittest import skipIf
 
-i = 0
-while '*' in s and '/' in s:
-    if s[i] == '*':
-        n[i] = int(n[i]) * int(n[i+1])
-        n.pop(i + 1)
-        s.pop(i)
-        print(n, s)
-        input()
-    elif s[i] == '/':
-        n[i] = int(n[i]) // int(n[i+1])
-        n.pop(i + 1)
-        s.pop(i)
-        print(n, s)
-        input()
-    i += 1
-print(n)
-print(s)
-for i in range(0, len(s)):
-    if s[i] == '+' and len(n) > 2:
-        n[i] = int(n[i]) + int(n[i+1])
-        n.pop(i + 1)
-        print(n, s)
-    elif s[i] == '-' and len(n) > 2:
-        n[i] += int(n[i]) - int(n[i+1])
-        n.pop(i + 1)
-        print(n, s)
-    if s[i] == '+' and len(n) <= 2:
-        res += int(n[0]) + int(n[1])
-    elif s[i] == '-' and len(n) <= 2:
-        res += int(n[0]) - int(n[1])
-print(res)
+
+def basic_calc(eq):
+    s_ind = []
+    s = []
+    n = []
+    nums = '0123456789'
+    for i in range(len(eq)):
+        if eq[i] not in nums:
+            s.append(eq[i])
+            s_ind.append(i)
+    i_last = 0
+    for i in s_ind:
+        n.append(eq[i_last:i])
+        i_last = i + 1
+    n.append(eq[i_last:len(eq)])
+    res = 0
+
+    i = 0
+    while ('*' in s) or ('/' in s) and s:
+        if len(n) > 1:
+            if len(s) <= i:
+                break
+            elif s[i] == '*':
+                print(s[i])
+                n[i] = int(n[i]) * int(n[i+1])
+                n.pop(i + 1)
+                s.pop(i)
+                print(n, s)
+                input()
+            print(i)
+            if s[i] == '/':
+                # print(s[i])
+                n[i] = int(n[i]) // int(n[i+1])
+                n.pop(i + 1)
+                s.pop(i)
+                print(n, s)
+                input()
+                i -= 1
+            i += 1
+        else:
+            return n[0]
+
+    print(n)
+    print(s)
+    i = 0
+    while len(n) > 1:
+        if s[i] == '+':
+            n[i] = int(n[i]) + int(n[i+1])
+            n.pop(i + 1)
+            s.pop(i)
+            # print(n, s)
+        elif s[i] == '-':
+            n[i] += int(n[i]) - int(n[i+1])
+            n.pop(i + 1)
+            s.pop(i)
+            # print(n, s)
+        i += 1
+    return n[0]
+print(basic_calc('3/2*2'))
