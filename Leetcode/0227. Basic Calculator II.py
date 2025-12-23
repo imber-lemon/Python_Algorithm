15c6633332226402c8f305da6ef96881175dd282
dddpros <imber.lemon09@gmail.com>
2025-12-23 08:45:58 +0300

    update

--- Leetcode/0227. Basic Calculator II.py
+++ Leetcode/0227. Basic Calculator II.py
@@ -1,61 +1,88 @@
-from unittest import skipIf
-
-
-def basic_calc(eq):
-    s_ind = []
-    s = []
-    n = []
-    nums = '0123456789'
-    for i in range(len(eq)):
-        if eq[i] not in nums:
-            s.append(eq[i])
-            s_ind.append(i)
-    i_last = 0
-    for i in s_ind:
-        n.append(eq[i_last:i])
-        i_last = i + 1
-    n.append(eq[i_last:len(eq)])
-    res = 0
-
-    i = 0
-    while ('*' in s) or ('/' in s) and s:
-        if len(n) > 1:
-            if len(s) <= i:
-                break
-            elif s[i] == '*':
-                print(s[i])
-                n[i] = int(n[i]) * int(n[i+1])
-                n.pop(i + 1)
-                s.pop(i)
-                print(n, s)
-                input()
-            print(i)
-            if s[i] == '/':
-                # print(s[i])
-                n[i] = int(n[i]) // int(n[i+1])
-                n.pop(i + 1)
-                s.pop(i)
-                print(n, s)
-                input()
-                i -= 1
-            i += 1
+# def basic_calc(eq):
+#     s_ind = []
+#     s = []
+#     n = []
+#     nums = '0123456789'
+#     for i in range(len(eq)):
+#         if eq[i] not in nums:
+#             s.append(eq[i])
+#             s_ind.append(i)
+#     i_last = 0
+#     for i in s_ind:
+#         n.append(eq[i_last:i])
+#         i_last = i + 1
+#     n.append(eq[i_last:len(eq)])
+#     res = 0
+#
+#     i = 0
+#     while ('*' in s) or ('/' in s) and s:
+#         if len(n) > 1:
+#             if len(s) <= i:
+#                 break
+#             elif s[i] == '*':
+#                 print(s[i])
+#                 n[i] = int(n[i]) * int(n[i+1])
+#                 n.pop(i + 1)
+#                 s.pop(i)
+#                 print(n, s)
+#                 input()
+#             print(i)
+#             if s[i] == '/':
+#                 # print(s[i])
+#                 n[i] = int(n[i]) // int(n[i+1])
+#                 n.pop(i + 1)
+#                 s.pop(i)
+#                 print(n, s)
+#                 input()
+#                 i -= 1
+#             i += 1
+#             if len(n) == 1:
+#                 return n[0]
+#
+#     print(n)
+#     print(s)
+#     i = 0
+#     while len(n) > 1:
+#         if s[i] == '+':
+#             n[i] = int(n[i]) + int(n[i+1])
+#             n.pop(i + 1)
+#             s.pop(i)
+#             # print(n, s)
+#         elif s[i] == '-':
+#             n[i] += int(n[i]) - int(n[i+1])
+#             n.pop(i + 1)
+#             s.pop(i)
+#             # print(n, s)
+#         i += 1
+#     return n[0]
+# print(basic_calc('6+2*4'))
+symb = '+-/*'
+eq = '6*2+4'
+last_n = 0
+res = 0
+s = 0
+n = 0
+for i in eq:
+    if i in symb:
+        s = i
+    else:
+        n = int(i)
+        if last_n != 0:
+            print(last_n, n, s)
+            if s == '*':
+                res += last_n * n
+                last_n = last_n * n
+            elif s == '/':
+                res += last_n // n
+                last_n = last_n // n
+            elif s == '+':
+                res += last_n + n
+                last_n = last_n + n
+            else:
+                res += last_n - n
+                last_n = last_n - n
         else:
-            return n[0]
-
-    print(n)
-    print(s)
-    i = 0
-    while len(n) > 1:
-        if s[i] == '+':
-            n[i] = int(n[i]) + int(n[i+1])
-            n.pop(i + 1)
-            s.pop(i)
-            # print(n, s)
-        elif s[i] == '-':
-            n[i] += int(n[i]) - int(n[i+1])
-            n.pop(i + 1)
-            s.pop(i)
-            # print(n, s)
-        i += 1
-    return n[0]
-print(basic_calc('3/2*2'))
+            last_n = n
+        print('res', n, eq[-1], res)
+        if n == int(eq[-1]):
+            print(res)
