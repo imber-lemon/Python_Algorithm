f1cfe2f6605dc702383f1b95b8a44bb9a64508b3
dddpros <imber.lemon09@gmail.com>
2026-01-27 11:48:15 +0300

    update

--- ООП/linked_list.py
+++ ООП/linked_list.py
@@ -1,6 +1,3 @@
-from types import new_class
-
-
 class Node:
     def __init__(self, value = None):
         self.value = value
@@ -13,7 +10,7 @@ class Linkedlist:
         current_node = self.head
         s = ''
         while current_node:
-            s += current_node + ','
+            s += current_node + ', '
             current_node = current_node.next
         return s
     def begin_insert(self, data):
@@ -25,14 +22,20 @@ class Linkedlist:
             self.head = new_node
     def end_insert(self, data):
         new_node = Node(data)
-        if self.head == None:
+        if not self.head:
             self.head = new_node
-    def update_data(self, value, data):
+    def update_data(self, pos, data):
         new_node = Node(data)
         current_node = self.head
         i = 0
-        while i <= value:
+        while i <= pos:
             current_node = current_node.next
             i += 1
         current_node = new_node
+llist = Linkedlist()
+llist.begin_insert(1)
+llist.begin_insert(1)
+llist.begin_insert(1)
+llist.update_data(3, 2)
+print(llist)
 
