bf47f4595f7cfee40cc6b905b046e24bbb9c80d5
dddpros <imber.lemon09@gmail.com>
2026-01-29 08:52:59 +0300

    update

--- ООП/linked_list.py
+++ ООП/linked_list.py
@@ -10,9 +10,12 @@ class Linkedlist:
         current_node = self.head
         s = ''
         while current_node:
-            s += str(current_node) + ', '
+            s += str(current_node.value) + ', '
             current_node = current_node.next
         return s
+    def inserter(self, lst):
+        for i in lst:
+            new_node = Node(i)
     def begin_insert(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -26,24 +29,34 @@ class Linkedlist:
         new_node = Node(data)
         current_node = self.head
         i = 0
-        while i <= index:
+        while i < index:
             current_node = current_node.next
             i += 1
-        current_node = new_node
+        current_node.value = new_node.value
     def insert_at_index(self, index, data):
         current_node = self.head
         i = 0
         for i in range(index):
             current_node = current_node.next
             i += 1
-        current_node = data
+        current_node = current_node.next
+        current_node.value = data
     def delete_head(self):
         self.head = self.head.next
     def delete_tail(self):
         current_node = self.head
         while current_node.next.next:
             current_node =  current_node.next
+        current_node.next = current_node.next.next
+    def count_len(self):
+        current_node = self.head
+        i = 0
+        while current_node:
+            current_node = current_node.next
+            i += 1
+        return i
 llist = Linkedlist()
-llist.begin_insert(1)
-llist.insert_at_index(1, 2)
+llist.inserter([1, 2, 3, 4])
 print(llist)
+# llist.delete_tail()
+# print(llist)
