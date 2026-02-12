1614fbcea442069db876b3b9d815d27c34782ede
dddpros <imber.lemon09@gmail.com>
2026-02-12 08:53:35 +0300

    print выводит все элементы правильно, но не добавляет в список

--- Leetcode/1669. Merge In Between Linked Lists.py
+++ Leetcode/1669. Merge In Between Linked Lists.py
@@ -1,13 +1,31 @@
-c = 0
-while c != a:
-    list1 = list1.next
-    c += 1
-cur1 = list1
-while c != b:
-    cur1 = cur1.next
-    c += 1
-while list2:
-    list1.val = list2.val
-    list1 = list1.next
-    list2 = list2.next
-list1 = cur1.val
+from classes import LinkedList
+
+def mergeInBetween(a, b, list1, list2):
+    c = 0
+    finish_lst = ListNode()
+    while c < a:
+        finish_lst = ListNode()
+        finish_lst.val = list1.val
+        print(list1.val)
+        finish_lst.next = ListNode()
+        finish_lst = finish_lst.next
+        list1 = list1.next
+        c += 1
+    while list2:
+        finish_lst = ListNode()
+        finish_lst.val = list2.val
+        print(list2.val)
+        finish_lst.next = ListNode()
+        finish_lst = finish_lst.next
+        list2 = list2.next
+        if list2 is not None:
+            list1 = list1.next
+        c += 1
+    while list1:
+        finish_lst = ListNode()
+        finish_lst.val = list1.val
+        print(list1.val)
+        finish_lst.next = ListNode()
+        finish_lst = finish_lst.next
+        list1 = list1.next
+    return finish_lst
