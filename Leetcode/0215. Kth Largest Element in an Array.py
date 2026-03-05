be51375e77f4b2bc56b29828131bf51fe955fa2c
dddpros <imber.lemon09@gmail.com>
2026-03-05 08:54:55 +0300

    update

--- Leetcode/0215. Kth Largest Element in an Array.py
+++ Leetcode/0215. Kth Largest Element in an Array.py
@@ -1,16 +1,26 @@
 def find_kth_element(nums, k):
-    lst = [0] * abs(max(nums))
+    lst = [0] * (max(nums) - min(nums) + 1)
     for i in nums:
         lst[abs(i)-1] += 1
     x = len(lst) - 1
     while k > 0:
         if lst[x] >= 1:
             if lst[x] > k:
-                return x + 1
+                print(x)
+                if x + 1 not in nums:
+                    return -(x + 1)
+                else:
+                    return x + 1
             else:
                 k -= lst[x]
                 if k == 0:
-                    return x + 1
+                    if x + 1 not in nums:
+                        return -(x + 1)
+                    else:
+                        return x + 1
         x -= 1
-    return x
-print(find_kth_element([-1, -1], 2))
+    if x not in nums:
+        return -x
+    else:
+        return x
+print(find_kth_element([99, 99], 2))
