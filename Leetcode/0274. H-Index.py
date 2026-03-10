7309ec47a56e458538b1c0b1313b3f55e841f7bf
dddpros <imber.lemon09@gmail.com>
2026-03-10 11:46:50 +0300

    update

---
+++ Leetcode/0274. H-Index.py
@@ -0,0 +1,9 @@
+def hIndex(citations):
+    h = 0
+    citations.sort(reverse=True)
+    print(citations)
+    for i in range(len(citations)):
+        if citations[i] >= i + 1:
+            h = i + 1
+    return h
+print(hIndex([3, 0, 6, 1, 5]))
