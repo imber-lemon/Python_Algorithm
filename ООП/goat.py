cc7a2fa1514ef11a23ffc9fe9b54de4b40818bc3
dddpros <imber.lemon09@gmail.com>
2026-03-31 11:46:32 +0300

    update

--- ООП/goat.py
+++ ООП/goat.py
@@ -1,10 +1,11 @@
-class goat:
+class Goat:
     def __init__(self, weight = 9, height = 1):
         self.weight = weight
         self.height = height
 
     def __str__(self):
-        s = 'Вес:', str(self.weight), 'Рост', str(self.height)
+        s = f"Вес: {self.weight}, Рост: {self.height}"
         return s
 
-    if self.weight <= 10 and
+masha = Goat(13, 4)
+print(masha)
