cc84f4f13274f74a42bf267d102a746b56dd27cb
dddpros <imber.lemon09@gmail.com>
2026-03-31 08:53:15 +0300

    update

--- Other/2026_03_19.py
+++ Other/2026_03_19.py
@@ -1,16 +1,25 @@
-import random as rd
+# import random as rd
+#
+#
+# def matches(players):
+#     d = dict()
+#     for i in players:
+#         op = rd.choice(players.remove(i))
+#         if i not in d:
+#             d[i] = []
+#         if op not in d:
+#             d[op] = []
+#         if op in d[i] or players[i] == op:
+#             op = rd.choice(players)
+#         else:
+#             d[i].append(op)
+#             d[op].append(players[i])
+#             print(d)
+#     # return d
 
 def matches(players):
     d = dict()
     for i in range(0, len(players)):
-        if players[i] not in d:
-            d[players[i]] = []
-        op = rd.choice(players[i+1:])
-        if op not in d[players[i]]:
-            d[players[i]].append(op)
-            if op not in d:
-                d[op] = []
-            d[op].append(players[i])
-            print(d)
-    #return d
-print(matches(["George Floyd", "Charlie Kirk", "Jeffrey Epstein", "P Diddy"]))
+        d[players[i]] = players.remove(players[i])
+    print(d)
+print(matches(["1 команда", "2 команда", "3 команда"]))
