613bc3b4f1835cead0423c55206e808f5e4ce5a2
dddpros <imber.lemon09@gmail.com>
2026-04-07 08:51:26 +0300

    update

---
+++ Leetcode/classes/print_matrix.py
@@ -0,0 +1,7 @@
+def print_matrix(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print('%2d' % matrix[i][j], end = ' ')
+        print()
+
+#print(print_matrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]))
