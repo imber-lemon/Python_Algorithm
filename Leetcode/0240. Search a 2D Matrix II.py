884ab31444da59eb3b12ee4378a706c59f1ef3b8
dddpros <imber.lemon09@gmail.com>
2026-04-07 08:51:10 +0300

    update

--- Leetcode/0240. Search a 2D Matrix II.py
+++ Leetcode/0240. Search a 2D Matrix II.py
@@ -1,11 +1,11 @@
+from classes.print_matrix import print_matrix
+
 def tar_in_matrix(matrix, target):
-    for i in range(0, len(matrix)):
-        print(matrix[i])
-    print(' ')
     for i in range(0, len(matrix)):
         for j in matrix[i]:
             if j == target:
                 return True
     return False
-
-print(tar_in_matrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5))
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+print_matrix(matrix)
+print(tar_in_matrix(matrix, 5))
