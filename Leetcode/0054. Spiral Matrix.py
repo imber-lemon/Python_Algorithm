3884b5a3cf9b09e4033e0d9d1bf2b549e3abdb31
dddpros <imber.lemon09@gmail.com>
2026-04-07 11:45:20 +0300

    update

--- Leetcode/0054. Spiral Matrix.py
+++ Leetcode/0054. Spiral Matrix.py
@@ -20,4 +20,4 @@ def spiral_matrix(matrix):
                 sp_m.append(matrix[-1][i])
         return sp_m
 print_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
-print(spiral_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
+print(spiral_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]))
