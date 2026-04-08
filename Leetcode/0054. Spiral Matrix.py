f150c8c8b2655d9570891e5e1c40b975c57b79e1
dddpros <imber.lemon09@gmail.com>
2026-04-08 08:48:20 +0300

    update

--- Leetcode/0054. Spiral Matrix.py
+++ Leetcode/0054. Spiral Matrix.py
@@ -1,14 +1,14 @@
 from classes.print_matrix import print_matrix
 
 def spiral_matrix(matrix):
+    print_matrix(matrix)
     sp_m = []
     num = matrix[0][0]
-    while len(sp_m) < len(matrix):
+    for l in range(len(matrix) // 2):
         if num == matrix[0][0]:
             for i in range(0, len(matrix[0])):
                 sp_m.append(matrix[0][i])
             num = matrix[0][-1]
-            # print(num)
         if num == matrix[0][-1]:
             for i in range(1, len(matrix)):
                 sp_m.append(matrix[i][-1])
@@ -16,6 +16,12 @@ def spiral_matrix(matrix):
         if num == matrix[-1][-1]:
             for i in range(len(matrix[0])-2, -1, -1):
                 sp_m.append(matrix[-1][i])
+            num = matrix[-1][0]
+        if num == matrix[-1][0]:
+            x = -2
+            while matrix[x][0] not in sp_m:
+                sp_m.append(matrix[x][0])
+                x -= 1
         return sp_m
-print_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
-print(spiral_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]))
+
+print(spiral_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
