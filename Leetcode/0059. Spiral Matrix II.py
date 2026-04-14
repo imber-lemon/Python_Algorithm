352dfa30ead84ad50cf1324dced058ea9767e4d2
dddpros <imber.lemon09@gmail.com>
2026-04-14 08:51:09 +0300

    update

---
+++ Leetcode/0059. Spiral Matrix II.py
@@ -0,0 +1,13 @@
+def spiral_matrixII(n):
+    matrix = [[]]
+    j = 0
+    for i in range(1, n**2+1):
+        if i % n == 0 and i != n**2:
+            matrix[j].append(i)
+            matrix.append([])
+            j += 1
+        else:
+            matrix[j].append(i)
+    return matrix
+print(spiral_matrixII(3))
+# 00, 01, 02, 12, 22, 21, 20, 10, 11
