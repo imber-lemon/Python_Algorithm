bd3e3d149113d1914c9bcbb350ccb07be1ba42c2
dddpros <imber.lemon09@gmail.com>
2026-04-21 08:49:52 +0300

    update

---
+++ ЕГЭ/2026_04_21.py
@@ -0,0 +1,29 @@
+# import sys
+#
+# sys.setrecursionlimit(10**6)
+#
+# def f(n):
+#     if n >= 21:
+#         return f(n - 8) + 1095
+#     else:
+#         return 10 * (G(n - 7) - 36)
+# def G(n):
+#     if n >= 22560:
+#         return n / 23 + 33
+#     else:
+#         return G(n + 11) - 4
+# print(f(548))
+
+f = [0] * 10000
+g = [0] * 10000
+for n in range(0, 100000):
+    if n < 22560:
+        g[n] = g[n + 11] - 4
+    else:
+        g[n] = n / 23 + 33
+    if n < 21:
+        f[n] = 10 * (g[n-7] - 36)
+    else:
+        f[n] = f[n-8] + 1095
+print(f[548])
+print(g)
