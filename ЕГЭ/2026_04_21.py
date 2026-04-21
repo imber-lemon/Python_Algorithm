9d68323480169d5e7396640e687907a88ffed94e
dddpros <imber.lemon09@gmail.com>
2026-04-21 11:44:32 +0300

    update

--- ЕГЭ/2026_04_21.py
+++ ЕГЭ/2026_04_21.py
@@ -15,15 +15,15 @@
 # print(f(548))
 
 f = [0] * 10000
-g = [0] * 10000
-for n in range(0, 100000):
+g = [0] * 100000
+for n in range(22600, -1, -1):
     if n < 22560:
         g[n] = g[n + 11] - 4
     else:
         g[n] = n / 23 + 33
+for n in range(0, 550):
     if n < 21:
         f[n] = 10 * (g[n-7] - 36)
     else:
         f[n] = f[n-8] + 1095
 print(f[548])
-print(g)
---
+++ ЕГЭ/2026_04_21_2.py
@@ -0,0 +1,2 @@
+lst = [i for i in open('17.txt')]
+print(lst)
