eef673301c48f108cadd5ce665de4c8a6fd9d1d3
dddpros <imber.lemon09@gmail.com>
2026-04-22 08:50:00 +0300

    update

--- Leetcode/0059. Spiral Matrix II.py
+++ Leetcode/0059. Spiral Matrix II.py
@@ -1,3 +1,6 @@
+from classes.print_matrix import print_matrix
+
+
 def spiral_matrixII(n):
     matrix = [[]]
     j = 0
@@ -8,30 +11,21 @@ def spiral_matrixII(n):
             j += 1
         else:
             matrix[j].append(i)
-    print(matrix)
+    # print_matrix(matrix)
     sp_m = []
-    l = 0 # счетчик отступа
-    num = matrix[0][0]
-    for l in range(len(matrix) // 2 + 1):
-        if num == matrix[l][l]:
-            for i in range(l, len(matrix[l]) - l):
-                sp_m.append(matrix[l][i])
-            num = matrix[l][-1-l]
-        if num == matrix[l][-l-1]:
-            for i in range(1 + l, len(matrix) - l):
-                sp_m.append(matrix[i][-1-l])
-            num = matrix[-1-l][-1-l]
-        if num == matrix[-1-l][-1-l]:
-            for i in range(len(matrix[0])-2-l, l-1, -1):
+    l = 0
+    while l <= (min(len(matrix), len(matrix[0])) - 1) // 2:
+        for i in range(l, len(matrix[0]) - l):
+            sp_m.append(matrix[l][i])
+        for i in range(l + 1, len(matrix) - l):
+            sp_m.append(matrix[i][-1-l])
+        if len(matrix) - 1 - l != l:
+            for i in range(len(matrix[0]) - 2 - l, l - 1, -1):
                 sp_m.append(matrix[-1-l][i])
-            num = matrix[-1-l][l]
-        if num == matrix[-1-l][l]:
-            x = -1-l
-            while matrix[x][l] not in sp_m:
-                sp_m.append(matrix[x][l])
-                x -= 1
-            num = matrix[x+1][1+l]
-            #print(num)
+        if len(matrix[0]) - 1 - l != l:
+            for i in range(len(matrix) - 2 - l, l, -1):
+                sp_m.append(matrix[i][l])
         l += 1
     return sp_m
-print(spiral_matrixII(10))
+
+print(spiral_matrixII(1))
