314f8a8fb5bb5dec46c33d840e9dcc3d366aa734
dddpros <imber.lemon09@gmail.com>
2026-04-29 08:48:23 +0300

    update

--- Other/2026_04_23.py
+++ Other/2026_04_23.py
@@ -1,11 +1,21 @@
-lst = [int(i) for i in open("17.txt")]
-print(lst)
-not_maxi = []
+lst = [int(i) for i in open('17.txt', encoding='utf-8-sig')]
+not_max = []
 maxi = 0
 while maxi % 100 != 28:
-    maxi = max(list(set(lst) - set(not_maxi)))
+    maxi = max(list(set(lst) - set(not_max)))
     if maxi % 100 != 28:
-        not_maxi.append(maxi)
-# for i in range(0, len(lst), 3):
-#     for x in range()
-print(maxi)
+        not_max.append(maxi)
+c = 0
+max_sum = 0
+sum_nums = 0
+for i in range(0, len(lst) - 2):
+    if sum(lst[i:i+3]) > 0 and sum(lst[i:i+3]) / 3 < maxi:
+        for x in lst[i:i+3]:
+            if 99 < abs(x) < 1000:
+                if sum(lst[i:i+3]) > max_sum:
+                    max_sum = sum(lst[i:i+3])
+                c += 1
+                break
+
+print("всего троек:", c)
+print("макс сумма:", max_sum)
