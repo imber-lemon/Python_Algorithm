90627dbcbb2d8c27f42bc340e280375ff1a2306c
dddpros <imber.lemon09@gmail.com>
2026-05-07 08:48:54 +0300

    update

--- Leetcode/0017. Letter Combinations of a Phone Number.py
+++ Leetcode/0017. Letter Combinations of a Phone Number.py
@@ -13,7 +13,7 @@ def letter_comb(digits):
     matrix = []
     for i in digits:
         matrix.append(nums[i])
-
+    print(matrix)
     if len(matrix) == 1:
         return matrix[0]
     elif len(matrix) == 2:
@@ -32,4 +32,28 @@ def letter_comb(digits):
                     for y in matrix[3]:
                         res.append(i + j + x + y)
     return res
-print(letter_comb("23"))
+#print(letter_comb("23"))
+
+def letter_comb_2(digits):
+    nums = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    }
+
+    res = [""]
+    for digit in digits:
+        new_res = []
+        for c in res:
+            for letter in nums[digit]:
+                new_res.append(c + letter)
+                print(new_res, res)
+                input()
+        res = new_res
+    return res
+print(letter_comb_2("23"))
